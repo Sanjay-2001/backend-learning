95ed48cf9991568b247b2d2cd888cd63561421fd
SanjayDutta <sanjaydutta976.com>
2024-12-19 18:03:26 +0530

    dot env

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const db = require("./db");
 const bodyParser = require("body-parser");
 const personRoutes = require("./routes/personRoutes");
 const menuRoutes = require("./routes/menuRoutes");
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("Welcome to my server");
@@ -16,6 +19,6 @@ app.get("/", function (req, res) {
 app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server running at port 3000");
 });
